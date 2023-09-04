c03d38daf7c7c41002506f78368b856ca507be4e
Ng Wen Hao Dennis <>
2023-09-04 22:41:58 +0800

    Finish basic section

--- basic/main.cpp
+++ basic/main.cpp
@@ -1,7 +1,62 @@
 #include <iostream>
+#include <vector>
+
+// All its attributes are public by default
+/*struct Cat {
+    int id;
+
+    Cat(int i) : id(i) {};
+
+    bool operator==(const Cat &other) const {
+        return id == other.id;
+    }
+};*/
+
+// All its attributes are private by default
+class Cat {
+public: // Set this as public
+    int id;
+
+    Cat(int i) : id(i) {};
+
+    bool operator==(const Cat &other) const {
+        return id == other.id;
+    }
+};
+
+void modify(std::vector<Cat>& cattos) {
+    for (int i = 0; i < cattos.size(); i++) {
+        cattos[i].id = 500;
+    }
+}
+
+void basics() {
+    // Create an object
+    Cat a = Cat(0);
+    Cat b(1);
+
+    std::vector<Cat> cats;
+    for (int i = 0; i < 3; i++) {
+        cats.push_back(Cat(i));
+    }
+
+    for (Cat& cat: cats) {
+        std::cout << "Cat id: " << cat.id << std::endl;
+        cat.id = 100;
+    }
+    std::cout << std::endl;
+
+    modify(cats);
+
+    std::cout << "Modified cats:" << std::endl;
+    for (auto cat: cats) {
+        std::cout << "Cat id: " << cat.id << std::endl;
+    }
+}
 
 int main() {
-    std::cout << "Hello, Basic!" << std::endl;
+    basics();
 
     return 0;
 }
+
